6b0ad693e3bcf36585fe4e8d0c73370cbb37d2ff
Prabha Sapkota <sapkotaprabha96@gmail.com>
2023-07-20 13:43:17 -0400

    Customized app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ from util_logger import setup_logger
 logger, logname = setup_logger(__name__)
 
 app_ui = ui.page_navbar(
-    shinyswatch.theme.lumen(),
+    shinyswatch.theme.flatly(),
     ui.nav(
         "Flights",
         ui.layout_sidebar(
