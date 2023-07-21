6b71d0f140a33f1dca5183231495a122f1db5bed
Prabha Sapkota <sapkotaprabha96@gmail.com>
2023-07-21 13:14:28 -0400

    Added Tips Dataset

--- data/app_data.py
+++ data/app_data.py
@@ -49,8 +49,8 @@ les_mis = pd.DataFrame(les_mis_data["links"])
 les_mis.to_excel("les_mis.xlsx")
 les_mis.to_csv("les_mis.csv")
 
-tips_df = sns.load_dataset("iris")
-tips_df.to_excel(data_folder.joinpath("iris.xlsx"))
+tips_df = sns.load_dataset("tips")
+tips_df.to_excel(data_folder.joinpath("tips.xlsx"))
 tips_df.to_csv(data_folder.joinpath("tips.csv"))
 
 
