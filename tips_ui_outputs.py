b22b0675b0869fb7fb1bee90e0faa976cfa1e6d0
Prabha Sapkota <sapkotaprabha96@gmail.com>
2023-07-21 15:42:30 -0400

    Reactive tips_outputs.py

--- tips_ui_outputs.py
+++ tips_ui_outputs.py
@@ -5,19 +5,19 @@ Choose the correct ui method for the type of output you want to display.
 Provide the exact name of the server function that will provide the output.
 """
 from shiny import ui
-
+from shinywidgets import output_widget
 
 def get_tips_outputs():
     return ui.panel_main(
-        ui.h2("Main Panel with Output (Not Yet Reactive)"),
+        ui.h2("Main Panel with Reactive Output"),
         ui.tags.hr(),
         ui.tags.section(
-            ui.h3("Tips Chart (Seaborn Scatter Plot)"),
-            ui.output_plot("tips_plot"),
+            ui.h3("Filtered Cars : Charts"),
+            output_widget("tips_output_widget1"),
             ui.tags.hr(),
-            ui.h3("Tips Table"),
+            ui.h3("Filtered Tips Table"),
             ui.output_text("tips_record_count_string"),
-            ui.output_table("tips_table"),
+            ui.output_table("tips_filtered_table"),
             ui.tags.hr(),
         ),
     )
