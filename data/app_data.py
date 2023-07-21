a3013ac80e6577c95e53d7f0fae86fc3ec05cca9
Prabha Sapkota <sapkotaprabha96@gmail.com>
2023-07-21 11:29:12 -0400

    Added new dataset for practice

--- data/app_data.py
+++ data/app_data.py
@@ -49,5 +49,10 @@ les_mis = pd.DataFrame(les_mis_data["links"])
 les_mis.to_excel("les_mis.xlsx")
 les_mis.to_csv("les_mis.csv")
 
+tips_df = sns.load_dataset("tips")
+tips_df.to_excel(data_folder.joinpath("tips.xlsx"))
+tips_df.to_csv(data_folder.joinpath("tips.csv"))
+
+
 # url="https://webpath/to/your/data.csv"
 # df=pd.read_csv(url)
