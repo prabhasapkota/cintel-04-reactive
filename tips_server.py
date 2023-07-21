7fec0287ced2645f685a4943aa8aed479c59917a
Prabha Sapkota <sapkotaprabha96@gmail.com>
2023-07-21 15:34:20 -0400

    Fixed tips_server.py

--- tips_server.py
+++ tips_server.py
@@ -1,59 +1,93 @@
-"""_summary_
-We are creating a new tab for our shiny app to display the tips dataset loaded earlier.
-The CSV file is loaded into a pandas DataFrame and the number of rows is counted.
-The server functions are defined in the get_iris_server_functions() function.
+""" 
+Purpose: Provide reactive output for the MT Cars dataset.
+
+- Use inputs from the UI Sidebar to filter the dataset.
+- Update reactive outputs in the UI Main Panel.
+
+Matching the IDs in the UI Sidebar and function/output names in the UI Main Panel
+to this server code is critical. They are case sensitive and must match exactly.
 
 """
 
-
 import pathlib
-from shiny import render
+from shiny import render , reactive
+import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sns
+from plotnine import aes, geom_point, ggplot, ggtitle
+from shinywidgets import render_widget
+import plotly.express as px
 
 from util_logger import setup_logger
 
 logger, logname = setup_logger(__name__)
 
 
+
+
 def get_tips_server_functions(input, output, session):
     """Define functions to create UI outputs."""
 
-    path_to_data = pathlib.Path(__file__).parent.joinpath("data").joinpath("tips.csv")
-    original_df = pd.read_csv(path_to_data)
-
-    # Use the len() function to get the number of rows in the DataFrame.
+    p = pathlib.Path(__file__).parent.joinpath("data").joinpath("mtcars.csv")
+    # logger.info(f"Reading data from {p}")
+    original_df = pd.read_csv(p)
     total_count = len(original_df)
 
-    @output
-    @render.table
-    def tips_table():
-        return original_df
+    reactive_df = reactive.Value()
+
+    @reactive.Effect
+    @reactive.event(input.TIPS_RANGE,)
+    def _():
+        df = original_df.copy()
+
+        input_range = input.TIPS_RANGE()
+        input_min = input_range[1]
+        input_max = input_range[10]
+
+        """
+        Filter the dataframe to just those greater than or equal to the min
+        and less than or equal to the max
+        Note: The ampersand (&) is the Python operator for AND
+        The column name is in quotes and is "mpg".
+        You must be familiar with the dataset to know the column names.
+        """
+
+        tips_filter = (df["tips"] >= input_min) & (df["tips"] <= input_max)
+        
+
+    # Set the reactive value
+    reactive_df.set(df)
+
+    
+
 
     @output
     @render.text
     def tips_record_count_string():
-        message = f"Showing {total_count} records"
-        logger.debug(f"filter message: {message}")
+        filtered_df = reactive_df.get()
+        filtered_count = len(filtered_df)
+        message = f"Showing {filtered_count} of {total_count} records"
+        # logger.debug(f"filter message: {message}")
         return message
+    
+    @output
+    @render.table
+    def tips_filtered_table():
+        filtered_df = reactive_df.get()
+        return filtered_df
+
 
     @output
-    @render.plot
-    def tips_plot():
-        """
-        Use Seaborn to make a quick scatterplot.
-        Provide a pandas DataFrame and the names of the columns to plot.
-        """
-        plt = sns.scatterplot(
-            data=original_df,
-            x="total bill",
-            y="tip",
-        )
-        return plt
+    @render_widget
+    def tips_output_widget1():
+        df = reactive_df.get()
+        plotly_express_plot = px.scatter(df, x="total bills", y="tips")
+        plotly_express_plot.update_layout(title="Tips with Plotly Express")
+        return plotly_express_plot
+
 
     # Return a list of function names for use in reactive outputs
-    functions = [
-        tips_table,
+    return [
         tips_record_count_string,
-        tips_plot,
-    ]
+        tips_filtered_table,
+        tips_output_widget1
+       ]
